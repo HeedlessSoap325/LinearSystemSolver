def8570cc2b51c43bdb4eb12003dfa00ddb09e40
HeedlessSoap325 <mathisjulian123@gmail.com>
2025-04-17 20:55:20 +0200

    Added Function to print System to Console

--- src/main/java/de/heedlesssoap/IOHandler.java
+++ src/main/java/de/heedlesssoap/IOHandler.java
@@ -1,5 +1,6 @@
 package de.heedlesssoap;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class IOHandler {
@@ -29,7 +30,40 @@ public class IOHandler {
             }
         }
 
+        printSystem(system, numVars, varNames);
+
         //TODO Gauss.solve(a, n);
         scanner.close();
+    }
+
+    public static void printSystem(double[][] system, int numVars , String[] varNames) {
+        for (int row = 0; row < numVars; row++) {
+            StringBuilder formattedRow = new StringBuilder();
+
+            for(int col = 0; col < numVars; col++) {
+                String sign;
+                if(col == 0){
+                    sign = system[row][col] >= 0.0 ? "" : "- ";
+                }else {
+                    sign = system[row][col] >= 0.0 ? " + " : " - ";
+                }
+
+                double value = Math.abs(system[row][col]);
+                if (value == 0.0) {
+                    formattedRow.append(String.format("%s%s", sign, "0"));
+                } else if (value == 1.0) {
+                    formattedRow.append(String.format("%s%s", sign, varNames[col]));
+
+                }else {
+                    String num = new DecimalFormat("#.##########").format(value);
+
+                    formattedRow.append(String.format("%s%s%s", sign, num, varNames[col]));
+                }
+            }
+            String num = new DecimalFormat("#.##########").format(system[row][numVars]);
+
+            formattedRow.append(String.format(" = %s", num));
+            System.out.println(formattedRow);
+        }
     }
 }
