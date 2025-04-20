108c8555ca7418e91e58fcf156d0023f06157364
HeedlessSoap325 <mathisjulian123@gmail.com>
2025-04-20 22:33:59 +0200

    Added Call to start-Function in Main and added Error handling

--- src/main/java/de/heedlesssoap/Main.java
+++ src/main/java/de/heedlesssoap/Main.java
@@ -1,7 +1,13 @@
 package de.heedlesssoap;
 
+import java.util.InputMismatchException;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            IOHandler.queryInput();
+        } catch (InputMismatchException e){
+            System.out.println("Invalid input, please try again!");
+        }
     }
 }
